c99d16e9bac68a320c57946b4a87d30db759981a
Exist Coding <ExistCoding@proton.me>
2023-02-11 17:40:31 +0100

    finished prototype styles for the buttons and started adding event listeners to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-let typed:string = "I am type string";
+//check for how many characters fit into display once, and store it until resize
 
-function logInput(input:string): void {
-    console.log(input);
+const buttons:string[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "/", "*", "+", "-"];
+
+buttons.forEach(button => {
+  const element = document.getElementById(button);
+  if (element) {
+    element.addEventListener("click", () => calculatorInput(button));
+  }
+  else {
+    alert("Error: Button not found")
+  }
+});
+
+function calculatorInput (button:string): void {
+        document.getElementById("display").textContent = document.getElementById("display").textContent?.concat(button);
+
 }
-
-logInput(typed);
