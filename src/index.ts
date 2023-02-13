cf122f896a736d65ec4c5d71c2865d130c8e31ab
Exist <ExistCoding@proton.me>
2023-02-13 20:41:46 +0100

    finished core functionality and styling for version 0.5, added compressed background images. next steps: 1. overhaul logic to not be reliant on evaluating strings. 2. overhaul style for actual 3d effects and overall cleaner look 3. Add more features, most importantly a clear button.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,38 @@
 //check for how many characters fit into display once, and store it until resize
+/*
+let numCharactersDisplay;
+
+document.addEventListener("DOMContentLoaded", checkDisplayOverflow);
+document.defaultView.addEventListener("resize", checkDisplayOverflow);
+
+function checkDisplayOverflow():void {
+  document.getElementById("display").textContent = "";
+  let dispClientWidth:number = document.getElementById("display").clientWidth;
+  let dispScrollWidth:number = document.getElementById("display").scrollWidth;
+  while (!dispScrollWidth > dispClientWidth) {
+
+}
+*/
+//  console.log("clientWidth: " + dispClientWidth);
+//  console.log("scrollWidth: " + dispScrollWidth);
+//  const isOverflowing:boolean = dispScrollWidth > dispClientWidth;
+//  console.log("isOverflowing:" + isOverflowing);
+
+//make it handle decimals properly
+//make it not use eval/not compute through strings
+//make it 3d?
+
+function calculatorInput(button: string): void {
+  const display = document.getElementById("display");
+  if (display) {
+    if (display.textContent === "0") {
+      display.textContent = "";
+    }
+    display.textContent = display.textContent!.concat(button);
+  } else {
+    console.error("Error: Element with id 'display' not found.");
+  }
+}
 
 const buttons:string[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "/", "*", "+", "-"];
 
@@ -12,7 +46,14 @@ buttons.forEach(button => {
   }
 });
 
-function calculatorInput (button:string): void {
-        document.getElementById("display").textContent = document.getElementById("display").textContent?.concat(button);
-
+function evaluate():void {
+  const display = document.getElementById("display");
+  if (display) {
+    try {display.textContent = eval(display.textContent!)}
+    catch {display.textContent = "Error"}
+  } else {
+    console.error("Error: Element with id 'display' not found.");
+  }
 }
+
+document.getElementById("=")?.addEventListener("click", evaluate);
