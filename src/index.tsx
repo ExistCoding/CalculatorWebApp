b9555f1de102247749af139a273e6ca2ec2b3478
Exist Coding <ExistCoding@proton.me>
2023-02-23 15:19:51 +0100

    refactored the timeout in Hello World function to use async await/promises

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function sayHello () {
+async function sayHello () {
   const display = document.getElementById("display");
   if (display) {
     display.textContent = "Hello World!";
-    setTimeout(() => {
-      display.textContent = "0";
-    }, 1000)
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    display.textContent = "0";
   }
   else {
     alert("Error: Display Element not found!")
   }
 }
 
+//Propably better implemented with factory function to clearer seperate UI and functionality.
+//In this way button could be implemented somewhere else with different functionality but same UI.
 const Button = () => {
   const handleClick = () => {
     sayHello();
