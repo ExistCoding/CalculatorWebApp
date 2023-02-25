e84cd566c2acf74c9e6ef646260ab70f8c050501
Exist Coding <ExistCoding@proton.me>
2023-02-25 20:36:00 +0100

    created demo for interactive components that pass Values between each other

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,51 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from "react";  //necessary?
+import ReactDOM from "react-dom/client"; 
 
-async function sayHello () {
-  const display = document.getElementById("display");
-  if (display) {
-    display.textContent = "Hello World!";
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    display.textContent = "0";
-  }
-  else {
-    alert("Error: Display Element not found!")
-  }
-}
+
 
 //Propably better implemented with factory function to clearer seperate UI and functionality.
 //In this way button could be implemented somewhere else with different functionality but same UI.
-const Button = () => {
+const GreetButton = ({greeting = "Hello World"} : {greeting?: string}) => {
+
   const handleClick = () => {
-    sayHello();
+    sayHello(greeting);
   }
-  return <button onClick={handleClick}>Hello, world!</button>;
+
+  async function sayHello (greeting) {
+    const display = document.getElementById("display");
+    if (display) {
+      display.textContent = greeting + "!";
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      display.textContent = "0";
+    }
+    else {
+      alert("Error: Display Element not found!")
+    }
+  }
+
+  return <button onClick={handleClick}>{greeting}</button>;
 };
 
 const rootNode = document.getElementById('test-root');
 const root = ReactDOM.createRoot(rootNode);
-root.render(<Button />);
+root.render(<GreetButton/>);
+
+
+/* 
+      Container for Scrimba code:
+*/
+
+function MyNavBar () {
+    return (
+        <nav>
+            <h1>Brand Name</h1>
+            <ul>
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+}
+
+ReactDOM.render(<MyNavBar />, document.getElementById("root"));
